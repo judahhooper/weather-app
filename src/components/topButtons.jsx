9f76250232f3f39539a969b40092cbdb5e371805
Judah Hooper <judahhooper@icloud.com>
2023-08-19 08:30:30 +0100

    cities array

--- src/components/topButtons.jsx
+++ src/components/topButtons.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const TopButtons = () => {
+
+    const cities = [
+        {
+            id:"1",
+            name:"london"
+        },
+        {
+            id:"2",
+            name:"los angeles"
+        },
+        {
+            id:"3",
+            name:"vienna"
+        },
+        {
+            id:"4",
+            name:"beirut"
+        },
+        {
+            id:"5",
+            name:"marrakesh"
+        }
+    ]
   return (
-    <div></div>
+    <div>hey</div>
   )
 }
 
