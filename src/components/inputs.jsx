e506c70f088774c36bde1daad449fc7d1d9119aa
Judah Hooper <judahhooper@icloud.com>
2023-08-19 09:01:47 +0100

    button styling

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { UilSearch,UilLocationPoint } from '@iconscout/react-unicons';
+import { UilSearch } from '@iconscout/react-unicons';
 
 const Inputs = () => {
   return (
-    <div>Inputs</div>
+    <div className='flex flex-row justify-center my-6 '>
+        <div className='flex flex-row width-3/4 items-center space-x-4'>
+            <input placeholder='Start typing...'type='text' className='text-xl font-light p-2 focus:outline-none capitalize placeholder:lowercase'></input>
+            <UilSearch size={20} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
+
+        </div>
+    </div>
   )
 }
 
