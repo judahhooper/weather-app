a3ff13a33831ce9e8e1a7ec977e68b7f713a2d34
Judah Hooper <judahhooper@icloud.com>
2023-08-20 07:26:34 +0200

    data fetcher working

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import weatherDataGetter from './resources/keys';
 
 
 function App() {
+
+    const fetchWeather = async () => {
+        const data = await weatherDataGetter('weather', {q: 'london'});
+        console.log(data);
+    };
+
+        fetchWeather();
 return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grey-400'>
         <TopButtons />
@@ -18,6 +26,8 @@ return (
         <Forecast title={"daily forecast"}/>
     </div>
 )
+
+
 }
 
 export default App;
--- src/resources/keys.js
+++ src/resources/keys.js
@@ -1,13 +1,16 @@
 const apiKey = '065c75edbe6e40dcb3723fa311578a92';
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}'
+const baseURL = 'https://api.openweathermap.org/data/2.5'
 
 
 //https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
 
 const weatherDataGetter = (infoType, searchParams) => {
-        const newURL = baseURL + '/' + infoType;
+        const newURL = new URL(baseURL + '/' + infoType);
         newURL.search = new URLSearchParams({...searchParams,  appid:apiKey});
 
         return fetch(newURL).then((response) => response.json())
+        .then((data) => data);
 }
+
+export default weatherDataGetter
