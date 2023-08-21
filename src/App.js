86c4536723013d4ba21819b48516b8e409bde666
Judah Hooper <judahhooper@icloud.com>
2023-08-21 16:47:51 +0200

    added test fetchweather to app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ function App() {
 
     const fetchWeather = async () => {
         const data = await getWeatherData();
+        console.log(data);
     }
+
+    fetchWeather();
 return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grey-400'>
         <TopButtons />
