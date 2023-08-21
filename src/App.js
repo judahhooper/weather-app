d78342552ad983123f075da097b60cd6a05dcceb
Judah Hooper <judahhooper@icloud.com>
2023-08-21 16:50:56 +0200

    pass query into getWeatherData for visibility

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import getWeatherData from './api-stuff/weatherService';
 function App() {
 
     const fetchWeather = async () => {
-        const data = await getWeatherData('weather');
+        const data = await getWeatherData('weather', {q: 'london'});
         console.log(data);
+        
     }
 
     fetchWeather();
+
 return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-grey-400'>
         <TopButtons />
