77af86e94578f342fb877baa6498f7bbb0477343
Judah Hooper <judahhooper@icloud.com>
2023-08-21 17:03:53 +0200

    icon and description logic from main

--- src/api-stuff/weatherService.js
+++ src/api-stuff/weatherService.js
@@ -11,7 +11,19 @@ const getWeatherData = (infoType, searchParams) => {
 };
 
 const formatCurrentWeather = (data) => {
+    const { 
+        coord: { lat , lon },
+        main: { temp, feels_like, temp_min, temp_max, humidity},
+        name, 
+        dt,
+        sys: { country, sunrise, sunset },
+        weather,
+        wind: {speed},
+    } = data;
+
+    const {main: details, icon} = weather[0];
     
+    return {lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, weather, speed}
 };
 
 const getFormattedWeatherData = async (searchParams) => {
